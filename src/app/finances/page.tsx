e305c222840f1f51de1a4c6196b8d58050d2974d
Yakov-Gapotchenko <yakov.gapotchenko@gmail.com>
2024-09-17 20:11:50 +0300

    Add select compoennt for User

--- src/app/finances/page.tsx
+++ src/app/finances/page.tsx
@@ -1,5 +1,14 @@
-import { FC } from "react";
-import { Finance } from "@prisma/client";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import prisma from "@/lib/prisma";
+import { Finance, User } from "@prisma/client";
 
 const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000";
 
@@ -8,15 +17,36 @@ const FinancePage = async () => {
     setTimeout(() => resolve("data"), 2000);
   });
 
-  const res = await fetch(`${baseUrl}/api/finances`, { cache: "no-store" });
-  if (!res.ok) {
+  const financesRes = await fetch(`${baseUrl}/api/finances`, {
+    cache: "no-store",
+  });
+  if (!financesRes.ok) {
     throw new Error("Failed to fetch finances");
   }
-  const finances: Finance[] = await res.json();
+  const finances: Finance[] = await financesRes.json();
+
+  const usersRes = await fetch(`${baseUrl}/api/users`, { cache: "no-store" });
+  if (!usersRes.ok) {
+    throw new Error("Failed to fetch users");
+  }
+  const users: User[] = await usersRes.json();
 
   return (
     <div>
       <h2>Financial Transactions</h2>
+      <Select>
+        <SelectTrigger className="w-[180px]">
+          <SelectValue placeholder="Select a fruit" />
+        </SelectTrigger>
+        <SelectContent>
+          <SelectGroup>
+            <SelectLabel>Users</SelectLabel>
+            {users.map((user) => (
+              <SelectItem value={user.id.toString()}>{user.name}</SelectItem>
+            ))}
+          </SelectGroup>
+        </SelectContent>
+      </Select>
       <ul>
         {finances.map((finance) => (
           <li key={finance.id}>
