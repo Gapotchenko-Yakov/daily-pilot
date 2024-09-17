ac7421554238d57895e36a813f2c8899e8c4e762
Yakov-Gapotchenko <yakov.gapotchenko@gmail.com>
2024-09-17 19:15:27 +0300

    Rewrite data seed.

--- prisma/seed.js
+++ prisma/seed.js
@@ -1,9 +1,44 @@
-// prisma/seed.js
 const { PrismaClient } = require("@prisma/client");
 
 const prisma = new PrismaClient();
 
+const startDate = new Date("2023-01-01");
+const dateList = Array.from({ length: 10 }, (_, i) => {
+  const date = new Date(startDate);
+  date.setDate(startDate.getDate() + i);
+  return date.toISOString().split("T")[0]; // Форматирование даты в YYYY-MM-DD
+});
+
+const taskDesriptions = [
+  "Go for a morning run",
+  "Call a friend to catch up",
+  "Buy groceries for the week",
+  "Clean and organize the desk",
+  "Read a chapter of a book",
+  "Cook a healthy dinner",
+  "Watch a movie or TV show",
+  "Do a short home workout",
+  "Water the plants",
+  "Plan the next day's schedule",
+  "Write in a journal",
+  "Take a walk in the park",
+  "Try a new recipe",
+  "Practice a hobby or craft",
+  "Meditate for 10 minutes",
+  "Tidy up the living room",
+  "Listen to a podcast episode",
+  "Review and respond to emails",
+  "Organize digital files on your computer",
+  "Prepare a list of goals for the week",
+];
+
 const main = async () => {
+  // delete old data
+  await prisma.task.deleteMany({});
+  await prisma.finance.deleteMany({});
+  await prisma.user.deleteMany({});
+
+  // add new data
   const users = await Promise.all([
     prisma.user.create({ data: { name: "Alice", email: "alice@example.com" } }),
     prisma.user.create({ data: { name: "Bob", email: "bob@example.com" } }),
@@ -18,9 +53,9 @@ const main = async () => {
         prisma.finance.create({
           data: {
             description: `Transaction ${i + 1}`,
-            amount: Math.random() * 1000,
+            amount: +(Math.random() * 1000).toFixed(2),
             type: i % 2 === 0 ? "INCOME" : "EXPENSE",
-            date: new Date(),
+            date: new Date(dateList[Math.floor(i / 2)]),
             userId: user.id,
           },
         })
@@ -32,7 +67,7 @@ const main = async () => {
         prisma.task.create({
           data: {
             title: `Task ${i + 1}`,
-            description: `Description for task ${i + 1}`,
+            description: taskDesriptions[i],
             dueDate: new Date(),
             status: "PENDING",
             userId: user.id,
--- src/app/tasks/page.tsx
+++ src/app/tasks/page.tsx
@@ -1,26 +1,31 @@
 import { FC } from "react";
 import { Task } from "@prisma/client";
+import Link from "next/link";
+import styles from "./page.module.css";
 
 const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000";
 
-const TaskPage = async () => {
-  await new Promise((resolve, reject) => {
+const TaskPage: FC = async () => {
+  await new Promise((resolve) => {
     setTimeout(() => resolve("data"), 2000);
   });
 
-  const res = await fetch(`${baseUrl}/api/tasks123`, { cache: "no-store" });
+  const res = await fetch(`${baseUrl}/api/tasks`, { cache: "no-store" });
   if (!res.ok) {
     throw new Error("Failed to fetch tasks");
   }
   const tasks: Task[] = await res.json();
 
   return (
-    <div>
-      <h2>Tasks</h2>
-      <ul>
+    <div className={styles.container}>
+      <h2 className={styles.title}>Tasks</h2>
+      <ul className={styles.list}>
         {tasks.map((task) => (
-          <li key={task.id}>
-            {task.description}: ${task.status}
+          <li key={task.id} className={styles.item}>
+            <Link href={`/tasks/${task.id}`} className={styles.link}>
+              <span>{task.description}</span>
+              <span>{task.status}</span>
+            </Link>
           </li>
         ))}
       </ul>
