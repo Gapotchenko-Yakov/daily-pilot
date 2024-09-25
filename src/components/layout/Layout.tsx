639985b30630356d690f52ff61d25371ec1b58f5
Yakov-Gapotchenko <yakov.gapotchenko@gmail.com>
2024-09-25 21:55:36 +0300

    Add sidebar links

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,11 +1,18 @@
 import { FC, ReactNode } from "react";
 import Link from "next/link";
 import {
+  ActivityIcon,
   BellIcon,
+  CalendarIcon,
+  CheckSquareIcon,
+  DollarSignIcon,
   HomeIcon,
   LogOutIcon,
   SettingsIcon,
+  TagIcon,
+  TargetIcon,
   UserIcon,
+  ClipboardCheckIcon,
 } from "lucide-react";
 
 import {
@@ -20,28 +27,65 @@ interface LayoutProps {
 }
 
 const headerLinks = [
-  { icon: <HomeIcon />, label: "Home", path: "/" },
-  { icon: <UserIcon />, label: "User", path: "/user" },
-  { icon: <SettingsIcon />, label: "Settings", path: "/settings" },
-  { icon: <BellIcon />, label: "Notifications", path: "/notifications" },
-  { icon: <LogOutIcon />, label: "Log Out", path: "/log-out" },
+  { Icon: HomeIcon, label: "Home", path: "/" },
+  { Icon: UserIcon, label: "User", path: "/user" },
+  { Icon: SettingsIcon, label: "Settings", path: "/settings" },
+  { Icon: BellIcon, label: "Notifications", path: "/notifications" },
+  {
+    Icon: LogOutIcon,
+    label: "Log Out",
+    path: "/log-out",
+  },
+];
+
+const sidebarLinks = [
+  {
+    Icon: CheckSquareIcon,
+    label: "Tasks",
+    path: "/tasks",
+  },
+  {
+    Icon: TargetIcon,
+    label: "Goals",
+    path: "/goals",
+  },
+  {
+    Icon: ActivityIcon,
+    label: "Habits",
+    path: "/habits",
+  },
+  {
+    Icon: DollarSignIcon,
+    label: "Transactions",
+    path: "/transactions",
+  },
+  {
+    Icon: TagIcon,
+    label: "Tags",
+    path: "/tags",
+  },
+  {
+    Icon: CalendarIcon,
+    label: "Reminders",
+    path: "/reminders",
+  },
 ];
 
 const Layout: FC<LayoutProps> = ({ children }) => {
   return (
     <div className="flex flex-col min-h-screen">
-      <header className="bg-gray-800 text-white p-4">
+      <header className="bg-alt px-4">
         <nav className="container mx-auto flex justify-between items-center">
-          <Link href="/" className="text-xl font-bold">
-            DailyPilot
+          <Link href="/" className="appname-link">
+            <ClipboardCheckIcon size={36} /> | DailyPilot
           </Link>
           <div className="flex gap-5">
-            {headerLinks.map(({ icon, label, path }) => (
+            {headerLinks.map(({ Icon, label, path }) => (
               <TooltipProvider>
                 <Tooltip>
                   <TooltipTrigger>
-                    <Link key={label} href={path} className="mx-2">
-                      {icon}
+                    <Link key={label} href={path} className="text-xl mx-2">
+                      {<Icon className="header-link" />}
                     </Link>
                   </TooltipTrigger>
                   <TooltipContent>
@@ -56,18 +100,14 @@ const Layout: FC<LayoutProps> = ({ children }) => {
 
       <main className="flex flex-1">
         <aside className="w-64 bg-gray-700 p-4">
-          <h2 className="text-lg font-semibold">Sidebar</h2>
-          <ul>
-            <li>
-              <Link href="/profile">Profile</Link>
-            </li>
-            <li>
-              <Link href="/settings">Settings</Link>
-            </li>
-            <li>
-              <Link href="/logout">Logout</Link>
-            </li>
-          </ul>
+          <div className="flex flex-col gap-5">
+            {sidebarLinks.map(({ Icon, label, path }) => (
+              <Link key={label} href={path} className="sidebar-link">
+                {<Icon />}
+                <span>{label}</span>
+              </Link>
+            ))}
+          </div>
         </aside>
 
         <div className="flex-1 p-6">{children}</div>
