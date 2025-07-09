357c4517eb38a9a139ce0a9361f87c9c03283540
Yakov-Gapotchenko <yakov.gapotchenko@gmail.com>
2025-07-09 14:17:08 +0300

    test123: test commitlint

--- src/widgets/layout/Layout.tsx
+++ src/widgets/layout/Layout.tsx
@@ -37,7 +37,7 @@ export default function Layout({ children }: LayoutProps) {
         <div>
             <Toolbar>
                 <Typography variant="h6" noWrap component="div">
-                    Мой сайт
+                    Мой сайт 123
                 </Typography>
             </Toolbar>
             <Divider />
