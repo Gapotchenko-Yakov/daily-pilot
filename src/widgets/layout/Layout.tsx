4d104a536d1b94afd9f7538e48924a74d2dd03ff
Yakov-Gapotchenko <yakov.gapotchenko@gmail.com>
2025-07-09 15:22:50 +0300

    chore: add husky to postinstall

--- src/widgets/layout/Layout.tsx
+++ src/widgets/layout/Layout.tsx
@@ -38,7 +38,7 @@ export default function Layout({ children }: LayoutProps) {
         <div>
             <Toolbar>
                 <Typography variant="h6" noWrap component="div">
-                    Мой сайт 1234
+                    Мой сайт 12345
                 </Typography>
             </Toolbar>
             <Divider />
