16f911ef8f82c76723152f8f461307a8f1a93d9f
Yakov-Gapotchenko <yakov.gapotchenko@gmail.com>
2025-07-16 18:30:50 +0300

    chore: update jest config

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,37 @@
 module.exports = {
+  // Общие настройки из обоих конфигов
+  preset: 'ts-jest',
   testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
+
+  // Настройки трансформации с Babel-конфигом
   transform: {
-    '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
+    '^.+\\.(js|jsx|ts|tsx)$': [
+      'babel-jest',
+      {
+        presets: [
+          ['@babel/preset-env', { targets: { node: 'current' } }],
+          [
+            '@babel/preset-react',
+            {
+              runtime: 'automatic',
+            },
+          ],
+          '@babel/preset-typescript',
+        ],
+        plugins: ['@babel/plugin-transform-runtime'],
+        babelrc: false,
+        configFile: false,
+      },
+    ],
   },
+
+  // Настройки алиасов
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy', // для обработки стилей
   },
+
+  testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
 };
