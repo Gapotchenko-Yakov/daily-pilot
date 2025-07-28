57a24cd237d365324df1eeae07a2ec8116b7c39f
Yakov-Gapotchenko <yakov.gapotchenko@gmail.com>
2025-07-28 19:04:56 +0300

    chore: setup tests

---
+++ jest.config.mjs
@@ -0,0 +1,16 @@
+import nextJest from 'next/jest.js';
+
+const createJestConfig = nextJest({
+  dir: './',
+});
+
+const customJestConfig = {
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  testEnvironment: 'jest-environment-jsdom',
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  moduleDirectories: ['node_modules', '<rootDir>/'],
+};
+
+export default createJestConfig(customJestConfig);
