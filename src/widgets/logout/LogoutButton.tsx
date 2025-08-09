e816a78a823721fbb69325af066eff6d86700362
Yakov-Gapotchenko <yakov.gapotchenko@gmail.com>
2025-08-09 18:10:50 +0300

    style(layout): restyle layout buttons

--- src/widgets/logout/LogoutButton.tsx
+++ src/widgets/logout/LogoutButton.tsx
@@ -1,8 +1,22 @@
 'use client';
+import { Button } from '@mui/material';
 import { signOut } from 'next-auth/react';
 
 export default function LogoutButton() {
   return (
-    <button onClick={() => signOut({ callbackUrl: '/login' })}>Выйти</button>
+    <Button
+      variant="contained"
+      color="inherit"
+      sx={(theme) => ({
+        backgroundColor: theme.palette.background.default,
+        color: theme.palette.primary.main,
+        '&:hover': {
+          backgroundColor: 'grey.100',
+        },
+      })}
+      onClick={() => signOut({ callbackUrl: '/login' })}
+    >
+      Выйти
+    </Button>
   );
 }
